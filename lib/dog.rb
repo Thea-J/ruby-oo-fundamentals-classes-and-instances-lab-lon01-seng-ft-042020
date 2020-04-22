dae61e077c8463ae03f3d76ba65d0155da7f4a9f
github username <github email address>
2020-04-22 15:11:18 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+#Class definition for a Dog class
+class Dog
+end
+
+#Instantiate 3 local variables
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+#Class definition for a Person class
+class Person
+end
+
+##Instantiate 2 local variables
+adele_goldberg = Person.new
+alan_kay = Person.new
